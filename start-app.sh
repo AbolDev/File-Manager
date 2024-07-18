53ffa235b5606d1225adeaf4baf982eb0d4a9811
Abol <107064351+AbolDev@users.noreply.github.com>
2024-07-18 18:51:41 +0330

    Update start-app.sh

--- start-app.sh
+++ start-app.sh
@@ -2,10 +2,17 @@
 
 app_file="app.py"
 
+# Start the Flask app in a detached screen session
 screen -dmS f-m-session python3 "$app_file"
 
+# Check if Flask started successfully
 if [ $? -eq 0 ]; then
-    echo "File Manager is running in screen session 'f-m-session'."
+    # Wait for a moment to allow Flask to start and print initial messages
+    sleep 2
+    # Get the output from screen logs that contains Flask startup messages
+    screen_logs=$(screen -S f-m-session -X logs)
+    # Print the Flask startup messages
+    echo "$screen_logs"
 else
     echo "Failed to start File Manager."
     exit 1
